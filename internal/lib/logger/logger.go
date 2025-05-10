a8b1b075580b75d7567c2a2c476418c812289744
romssc <xoticdollarsign@outlook.com>
2025-05-11 00:37:15 +0300

    new modes added

--- internal/lib/logger/logger.go
+++ internal/lib/logger/logger.go
@@ -1,23 +1,32 @@
 package logger
 
 import (
-	"log"
 	"log/slog"
 	"os"
+
+	"infopuller/internal/utils/config"
 )
 
 type Logger struct {
 	Log *slog.Logger
 }
 
-func New() *Logger {
-	var log *log.Logger
+func New(c config.Config) *Logger {
+	var log *slog.Logger
 
-	switch {
+	switch c.LogMode {
 	case "local":
 		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 			Level: slog.LevelDebug,
 		}))
+
+	case "dev":
+		log = slog.New(slog.NewJSONHandler(nil, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+
+	case "prod":
+		// TODO: LOGGER FOR PROD
 	}
 
 	return &Logger{
