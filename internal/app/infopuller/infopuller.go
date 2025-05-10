764fd1f551cf7084800c66e5ce0af78bcb3bd997
romssc <xoticdollarsign@outlook.com>
2025-05-11 00:08:23 +0300

    grpc server and infopuller server created

--- internal/app/infopuller/infopuller.go
+++ internal/app/infopuller/infopuller.go
@@ -1,9 +1,19 @@
 package infopuller
 
+import (
+	"google.golang.org/grpc"
+
+	infopullerpb "github.com/kinofinder/proto/gen/go/infopuller"
+)
+
 type App struct {
 }
 
 func New() *App {
+	grpcs := grpc.NewServer()
+
+	infopullerpb.RegisterInfoPullerServer(grpcs, nil)
+
 	return &App{}
 }
 
