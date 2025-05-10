457f7974a8128f70123a9ea5766275bae7ec4c70
romssc <xoticdollarsign@outlook.com>
2025-05-11 01:10:10 +0300

    run func finished

--- internal/app/infopuller/infopuller.go
+++ internal/app/infopuller/infopuller.go
@@ -1,26 +1,55 @@
 package infopuller
 
 import (
+	"infopuller/internal/utils/config"
+	"log/slog"
+	"net"
+
 	"google.golang.org/grpc"
 
 	infopullerpb "github.com/kinofinder/proto/gen/go/infopuller"
 )
 
 type App struct {
+	Server *grpc.Server
+
+	Log *slog.Logger
+
+	Config config.Config
 }
 
-func New() *App {
+func New(log *slog.Logger, config config.Config) *App {
 	grpcs := grpc.NewServer()
 
 	infopullerpb.RegisterInfoPullerServer(grpcs, &Handlers{})
 
-	return &App{}
+	return &App{
+		Server: grpcs,
+
+		Log: log,
+
+		Config: config,
+	}
 }
 
-func (a *App) Run() {}
+func (a *App) Run() error {
+	l, err := net.Listen(a.Config.Network, a.Config.Address)
+	if err != nil {
+		return err
+	}
+
+	err = a.Server.Serve(l)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func (a *App) Shutdown() {}
 
 type Handlers struct {
 	infopullerpb.UnimplementedInfoPullerServer
+
+	Log *slog.Logger
 }
