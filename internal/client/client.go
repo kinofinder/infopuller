12118b377dcab6ac09929b4420675a01ae39df98
romssc <xoticdollarsign@outlook.com>
2025-05-11 02:52:31 +0300

    client random handler completed

--- internal/client/client.go
+++ internal/client/client.go
@@ -1,13 +1,18 @@
 package client
 
 import (
+	"context"
+	"fmt"
 	"infopuller/internal/utils/config"
+	"io"
 	"log/slog"
 	"net/http"
 )
 
+// TODO: FIGURE OUT BETTER ERRORS
+
 type Client struct {
-	Client http.Client
+	http.Client
 
 	Log *slog.Logger
 
@@ -19,6 +24,8 @@ func New(log *slog.Logger, c config.Config) *Client {
 		Timeout: c.Client.Timeout,
 	}
 
+	// TODO: DEBUG LOG CLIENT START
+
 	return &Client{
 		Client: client,
 
@@ -29,5 +36,43 @@ func New(log *slog.Logger, c config.Config) *Client {
 }
 
 func (c *Client) Shutdown() {
-	c.Client.CloseIdleConnections()
+	c.CloseIdleConnections()
+}
+
+func (c *Client) Random() ([]byte, error) {
+	req, err := http.NewRequestWithContext(context.Background(), http.MethodGet, c.Config.Client.RandomURL, nil)
+	if err != nil {
+		// TODO: LOG ERROR
+
+		return nil, err
+	}
+
+	req = addHeaders(req, c.Config.Client.KinopoiskAPIKey)
+
+	resp, err := c.Do(req)
+	if err != nil {
+		// TODO: LOG ERROR
+
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		// TODO: LOG ERROR
+
+		return nil, fmt.Errorf("request failed: %v", resp.StatusCode)
+	}
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
+
+func addHeaders(req *http.Request, key string) *http.Request {
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("X-API-KEY", key)
+
+	return req
 }
