5f29c09b975bf7c4e57b0fcef39a3a02cae70b4a
romssc <xoticdollarsign@outlook.com>
2025-05-11 00:23:16 +0300

    add: basic funcs for logger init

--- internal/lib/logger/logger.go
+++ internal/lib/logger/logger.go
@@ -1,1 +1,26 @@
 package logger
+
+import (
+	"log"
+	"log/slog"
+	"os"
+)
+
+type Logger struct {
+	Log *slog.Logger
+}
+
+func New() *Logger {
+	var log *log.Logger
+
+	switch {
+	case "local":
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	}
+
+	return &Logger{
+		Log: log,
+	}
+}
