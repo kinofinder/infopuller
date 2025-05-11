199f5da31f38e5520da6d300b8c0962a75f311e4
romssc <xoticdollarsign@outlook.com>
2025-05-11 17:02:06 +0300

    logging removed

--- internal/lib/logger/logger.go
+++ internal/lib/logger/logger.go
@@ -66,13 +66,6 @@ func loadLogFile(dir string, name string) (*os.File, error) {
 }
 
 func (l *Logger) Shutdown() {
-	const op = "logger.Shutdown()"
-
-	l.Debug(
-		"stopping the logger",
-		slog.String("op", op),
-	)
-
 	if l.LogFile != nil {
 		l.LogFile.Close()
 	}
