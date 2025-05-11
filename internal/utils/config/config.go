a182ad48096e264cf45fcc40ef48e5b4a7352215
romssc <xoticdollarsign@outlook.com>
2025-05-11 15:25:16 +0300

    config loading completed

--- internal/utils/config/config.go
+++ internal/utils/config/config.go
@@ -1,26 +1,57 @@
 package config
 
-import "time"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+	"github.com/joho/godotenv"
+)
 
 type Config struct {
-	Network string
-	Address string
+	Network string `yaml:"network" env-default:"tcp"`
+	Address string `yaml:"address" env-default:"0.0.0.0:5430"`
 
-	Client ClientConfig
+	Client ClientConfig `yaml:"client"`
 
-	LogMode      string
-	LogDirectory string
+	LogMode      string `yaml:"log_mode" env-default:"local"`
+	LogDirectory string `yaml:"log_directory" env-default:"log"`
 }
 
 type ClientConfig struct {
 	KinopoiskAPIKey string
-	Timeout         time.Duration
+	Timeout         time.Duration `yaml:"timeout" env-default:"10s" env-upd:"true"`
 
-	RandomURL string
+	RandomURL string `yaml:"random_url" env-default:"https://api.kinopoisk.dev/v1.4/movie/random" env-upd:"true"`
 }
 
-func New() Config {
+func New() (Config, error) {
 	// TODO: DEBUG LOG CONFIG LOAD
 
-	return Config{}
+	err := godotenv.Load()
+	if err != nil {
+		return Config{}, err
+	}
+
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		return Config{}, fmt.Errorf("config path not specified")
+	}
+
+	apiKey := os.Getenv("KINOPOISK_API_KEY")
+	if apiKey == "" {
+		return Config{}, fmt.Errorf("api key not specified")
+	}
+
+	var config Config
+
+	err = cleanenv.ReadConfig(configPath, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config.Client.KinopoiskAPIKey = apiKey
+
+	return Config{}, nil
 }
